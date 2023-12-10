7e6984ce7e25043056818a6383fb3d301670f7fd
Sergey Zwezdin <sergey@zwezdin.com>
2023-12-10 17:21:10 +0300

    fix(core): fixing ESM issue

--- src/verifyConditions.ts
+++ src/verifyConditions.ts
@@ -1,6 +1,5 @@
+import * as SemanticReleaseError from "@semantic-release/error";
 import { Config, resolveConfig } from "./config";
-
-const SemanticReleaseError = require("@semantic-release/error");
 
 async function verifyConditions(config: Config) {
   const options = resolveConfig(config);
