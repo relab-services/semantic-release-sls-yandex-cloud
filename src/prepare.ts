8b6975259ec7e718c7bd78b4fa4991539b096a4e
Sergey Zwezdin <sergey@zwezdin.com>
2023-12-10 16:55:16 +0300

    chore(core): cleanup debug info

--- src/prepare.ts
+++ src/prepare.ts
@@ -1,22 +1,16 @@
-import { PrepareContext } from "semantic-release";
 import * as path from "path";
 import { existsSync } from "fs";
 import * as fs from "fs/promises";
 import { Config, resolveConfig } from "./config";
 
-async function prepare(config: Config, context: PrepareContext) {
+async function prepare(config: Config) {
   const options = resolveConfig(config);
   const configPath = path.resolve(process.env.HOME ?? "", ".config/yandex-cloud/config.yaml");
 
-  context.logger.log(`Config path: ${configPath}`);
-
-  context.logger.log(`Base path ${path.dirname(configPath)} exists: ${existsSync(path.dirname(configPath))}`);
   if (!existsSync(path.dirname(configPath))) {
     await fs.mkdir(path.dirname(configPath), {
       recursive: true,
     });
-
-    context.logger.log(`Base dir ${path.dirname(configPath)} created`);
   }
 
   const content = `current: default
@@ -27,9 +21,7 @@ profiles:
     folder-id: ${options.folderId}
     compute-default-zone: ${options.defaultZone}`;
 
-  context.logger.log(`Writing configuration to ${configPath}\n${content}`);
   await fs.writeFile(configPath, content, { encoding: "utf-8" });
-  context.logger.log(`Writing configuration to ${configPath} done`);
 }
 
 export { prepare };
