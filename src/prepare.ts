bab5fcc6be28f2ded94f1aacbd95befa6c0e31d7
Sergey Zwezdin <sergey@zwezdin.com>
2023-12-10 16:14:30 +0300

    feat(core): prepare yandex config file

--- src/prepare.ts
+++ src/prepare.ts
@@ -1,8 +1,25 @@
-import { PrepareContext } from "semantic-release";
 import { Config, resolveConfig } from "./config";
+import * as path from "path";
+import { existsSync } from "fs";
+import * as fs from "fs/promises";
 
-async function prepare(config: Config, context: PrepareContext) {
-  context.logger.log(`Prepare: ${JSON.stringify(JSON.stringify(resolveConfig(config)))}`);
+async function prepare(config: Config) {
+  const options = resolveConfig(config);
+  const configPath = path.resolve("~/.config/yandex-cloud/config.yaml");
+
+  if (!existsSync(path.basename(configPath))) {
+    await fs.mkdir(path.basename(configPath));
+  }
+
+  const content = `current: default
+profiles:
+  default:
+    token: ${options.token}
+    cloud-id: ${options.cloudId}
+    folder-id: ${options.folderId}
+    compute-default-zone: ${options.defaultZone}`;
+
+  await fs.writeFile(configPath, content, { encoding: "utf-8" });
 }
 
 export { prepare };
